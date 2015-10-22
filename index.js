30d8206244ceadfe81622630456fe3db9c0e9def
Paul <deprazz@gmail.com>
2015-10-22 16:13:38 -0200

    Bug fix

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use('/imp', baseApp);
 baseApp.authenticate = require('./app/authenticate').auth;
 
 // allow cross-domain. see http://www.w3.org/TR/cors/
-app.use(function(req, res, next) {
+baseApp.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
@@ -27,7 +27,7 @@ app.use(function(req, res, next) {
     else {
         next();
     }
-}, baseApp);
+});
 
 consign({cwd: 'app'})
     .include('auth')
